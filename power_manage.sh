35881d534c1af3c09fa9023c3cc517d8b8b2891b
smallfish01 <xiaoyu712@hotmail.com>
2021-10-08 11:34:51 +0800

    Update power_manage.sh
    该脚本用于通过DELL刀片服务器iDRAC接口，对服务器进行批量远程开关机操作。<br><br>
    使用方法：<br><br>
    
    1.将服务器iDRAC接口IP地址写入到文件ip.txt中，一行一个IP；<br>
    2.执行脚本后，根据提示进行操作：<br>
    输入数字1，对所要操作的服务器IP进行网络连通性测试，确认IP都可达；<br>
    输入数字2，查询当前要操作的服务器电源状态，是Power ON 还是Power OFF；<br>
    输入数字3，一键关闭所有服务器电源；<br>
    输入数字4，一键开启所有服务器电源；<br>
    输入数字5，一键对所有服务器进行重启；<br>
    输入数字6，一键查看刀箱电源状态；<br>
    输入数字7，一键关闭刀机箱电源；<br>
    输入数字8，一键开启刀箱电源；<br>
    输入数字0，退出。<br><br>
    
    3.特别注意，对于MX7000刀片服务器，由于主机箱不支持sshpass,expect命令，所以无法使用该脚本远程开关主机箱电源，需要手动ssh到服务器操作。<br>

--- power_manage.sh
+++ power_manage.sh
@@ -1,45 +1,43 @@
 #!/bin/bash
 # For Power on/down/reset Dell Server
 # Author Jun Yu
-# V1.0
+# V1.1
 # 该脚本用于检查DELL服务器电源状态，远程批量自动开关机。服务器必须有iDRAC功能，配置IP地址，账号密码，可用于远程ssh.
-
-#set -x
+# Dell MX7000不支持sshpass,expect命令，无法通过批量开关机脚本，只能通过手动ssh
 pub(){
 expect_dir="`dirname $0`"
 server_ip="${expect_dir}/ip.txt"
-pass="password"
-chass_ip="192.168.21.2"
+pass="your iDRAC password"
+#chassis_ip="dell chassis ip" 
 }
 
 ping_test(){
-# Ping servers
     sed -i '/^$/d' ${server_ip}
     num=`cat ${server_ip}|grep -v "^#"| wc -l`
     for ((i=1;i<=${num};i++))
     do
        ip=`cat ${server_ip} | grep -v "^#" |head -$i | tail -1|awk -F " " '{print $1}'`
 #       sed -i "/${ip}/d" ${ssh_know}
-       ping -c 4 -W 1  $ip >/dev/null 2>&1
+       ping -c 2 -W 1  $ip >/dev/null 2>&1
        if [ $? -eq 0 ]
        then
             echo -e "\033[35m$ip network is reachable!!!\033[0m"
        else
-            echo "$ip network is unreachable!!!"
+            echo "$ip network is unreachable!!!"    
        fi
     done
 
 }
 
 power_status(){
-# Check Dell server power status
      sed -i '/^$/d' ${server_ip}
      num=`cat ${server_ip}|grep -v "^#"| wc -l`
      for ((i=1;i<=${num};i++))
      do
      ip=`cat ${server_ip} | grep -v "^#" |head -$i | tail -1|awk -F " " '{print $1}'`
      echo "Show $ip power status"
-     status="`sshpass -p $pass ssh -o StrictHostKeyChecking=no root@$ip racadm serveraction powerstatus`"
+     status="`sshpass -p $pass ssh -o StrictHostKeyChecking=no root@$ip racadm serveraction powerstatus`" 
+    # status="`sshpass -p tc123456 ssh -o StrictHostKeyChecking=no root@$ip cat /etc/issue`"
      echo $status
      printf "$(date) Server $ip $status\n" >>/root/powerstatus1.txt
      done
@@ -53,6 +51,7 @@ power_on(){
      ip=`cat ${server_ip} | grep -v "^#" |head -$i | tail -1|awk -F " " '{print $1}'`
      echo "Show $ip power status"
      status="`sshpass -p $pass ssh -o StrictHostKeyChecking=no root@$ip racadm serveraction powerup`"
+    # status="`sshpass -p tc123456 ssh -o StrictHostKeyChecking=no root@$ip cat /etc/issue`"
      echo $status
      printf "$(date) Server $ip $status\n" >>/root/powerstatus1.txt
      done
@@ -66,6 +65,7 @@ power_off(){
      ip=`cat ${server_ip} | grep -v "^#" |head -$i | tail -1|awk -F " " '{print $1}'`
      echo "Show $ip power status"
      status="`sshpass -p $pass ssh -o StrictHostKeyChecking=no root@$ip racadm serveraction powerdown`"
+    # status="`sshpass -p tc123456 ssh -o StrictHostKeyChecking=no root@$ip cat /etc/issue`"
      echo $status
      printf "$(date) Server $ip $status\n" >>/root/powerstatus1.txt
      done
@@ -79,20 +79,43 @@ reboot(){
      ip=`cat ${server_ip} | grep -v "^#" |head -$i | tail -1|awk -F " " '{print $1}'`
      echo "Show $ip power status"
      status="`sshpass -p $pass ssh -o StrictHostKeyChecking=no root@$ip racadm serveraction powercycle`"
+    # status="`sshpass -p tc123456 ssh -o StrictHostKeyChecking=no root@$ip cat /etc/issue`"
      echo $status
      printf "$(date) Server $ip $status\n" >>/root/powerstatus1.txt
      done
 }
 
-
-power_on_chassis(){
+power_status_chassis(){
      for ip in $chassis_ip
      do
      echo "Show Poweredge chassis power status"
      sshpass -p $pass ssh -o StrictHostKeyChecking=no root@$ip racadm getsysinfo -c > chassis.txt
      stat=`awk '/Power Status/{print}' chassis.txt`
      echo $stat
-     printf "$(date) Server $ip $status\n" >>/root/powerstatus1.txt
+     printf "$(date) Server $ip $status\n" >>/root/chassis_powerstatus.txt
+     done
+}
+
+
+power_down_chassis(){
+     for ip in $chassis_ip
+     do
+     echo "Show Poweredge chassis power status"
+     sshpass -p $pass ssh -o StrictHostKeyChecking=no root@$ip racadm chassisaction powerdown > chassis.txt
+     stat=`awk '/Power Status/{print}' chassis.txt`
+     echo $stat
+     printf "$(date) Server $ip $status\n" >>/root/chassis_powerstatus.txt
+     done
+}
+
+power_on_chassis(){
+     for ip in $chassis_ip
+     do
+     echo "Show Poweredge chassis power status"
+     sshpass -p $pass ssh -o StrictHostKeyChecking=no root@$ip racadm chassisaction powerup > chassis.txt
+     stat=`awk '/Power Status/{print}' chassis.txt`
+     echo $stat
+     printf "$(date) Server $ip $status\n" >>/root/chassis_powerstatus.txt
      done
 }
 
@@ -106,7 +129,9 @@ do
   echo -e "\033[31m [3].一键运行所有服务器关机. \033[0m"
   echo -e "\033[31m [4].一键运行所有服务器开机. \033[0m"
   echo -e "\033[31m [5].一键运行所有服务器重启. \033[0m"
-  echo -e "\033[31m [6].一键查看刀片服务器主机电源状态.\033[0m"
+  echo -e "\033[31m [6].一键查看刀电源状态.\033[0m"
+  echo -e "\033[31m [7].一键关闭刀箱电源.\033[0m"
+  echo -e "\033[31m [8].一键开启刀箱电源，MX7000除外.\033[0m"
   echo -e "\033[32m [0].退出程序. \033[0m"
   echo -e "\033[32m ******************************************\n \033[0m"
 
@@ -135,6 +160,14 @@ case $action in
      ;;
   6)
      pub
+     power_status_chassis
+     ;;
+  7)
+     pub
+     power_down_chassis
+     ;;
+  8)
+     pub
      power_on_chassis
      ;;
   0)
@@ -143,7 +176,7 @@ case $action in
     ;;
 
   *)
-    echo "抱歉，您的选择有误!"
+    echo "抱歉，您的选择有误，请输入正确数字！"
     ;;
 
 esac
